f5f8caee7c6b329b98a0e0bb85a1ea8c79fb4707
JoeZhou1234 <87938075+JoeZhou1234@users.noreply.github.com>
2022-12-13 09:13:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 """
-This is where all the functions are executed
+This is where all the code is executed.
 """
 
 if __name__ == '__main__':
-    print("hi")
+
+    # import get_permissions
+    import move_files
+
+    # (For testing purposes)
+    bucket_name = "joe-test-bucket"
+    directory_path = r"C:\Users\Joe\Test Sample"
+
+    # directory_path = get_permissions.obtain_directory_path()
+    # if directory_path != "":
+    #     get_permissions.obtain_all_file_permissions(directory_path)
+
+    # (For testing purposes)
+    # get_permissions.test.close()
+
+    # Test Part 1
+    # move_files.move_local_to_s3(directory_path, bucket_name)
+
+    # Test Part 2
+    # sub_directory_path = r"C:\Users\joe\test.txt"
+    # move_files.move_local_to_s3(sub_directory_path, bucket_name)
+
